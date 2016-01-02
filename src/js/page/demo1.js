1606d02deaa29725a612a14725991cc799227095
VaJoy Larn <vajoy@qq.com>
2016-01-03 01:13:49 +0800

    modify

--- src/js/page/demo1.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by vajoylan on 2015/12/17.
- */
-import List from '../component/List';
-import {bootstrap} from 'angular2/platform/browser'
-
-document.addEventListener('DOMContentLoaded', function() {
-    bootstrap(List);
-});
